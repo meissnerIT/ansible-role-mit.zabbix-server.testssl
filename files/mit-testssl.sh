168c660ee0bd01b32db0a673d567416a0fcc00d0
Markus Meissner <markus.meissner@meissner.IT>
2020-05-07 09:34:09 +0200

    Minor

--- files/mit-testssl.sh
+++ files/mit-testssl.sh
@@ -2,12 +2,16 @@
 #
 # Distributed via ansible - mit.zabbix-server.testssl
 #
-# v2017-08-24-1
+# v2020-03-25-1
 
 set -e
 
 # Ensure exit-code 0 even if grep fails
+
 # cat /tmp/testssl.sh-result-www.meissner.it \
 /usr/local/share/testssl.sh/testssl.sh --vulnerable --color 0 --quiet $1 \
     | grep "(NOT ok)" || true
+
+/usr/local/share/testssl.sh/testssl.sh --protocols --color 0 --quiet $1 \
+    | egrep "\((NOT ok|deprecated)\)" | paste -s -d, | xargs || true
 
